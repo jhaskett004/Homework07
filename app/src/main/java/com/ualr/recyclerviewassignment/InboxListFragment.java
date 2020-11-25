1e42d2e5ac3008312b890ea62886c6bcc57c5cbe
jhaskett004 <jhaskett@ualr.edu>
2020-11-25 15:33:39 -0600

    Initial commit

---
+++ app/src/main/java/com/ualr/recyclerviewassignment/InboxListFragment.java
@@ -0,0 +1,47 @@
+package com.ualr.recyclerviewassignment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+public class InboxListFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private AdapterListInbox mAdapter;
+    private FloatingActionButton mFAB;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.inbox_list_fragment_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            recyclerView = view.findViewById(R.id.recyclerView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            recyclerView.setHasFixedSize(true);
+            List<Inbox> items = DataGenerator.getInboxData(getActivity());
+            mAdapter = new AdapterListInbox(getActivity(), items);
+            recyclerView.setAdapter(mAdapter);
+            mFAB = view.findViewById(R.id.fab);
+            mFAB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mAdapter.addNewItem();
+                    recyclerView.scrollToPosition(0);
+                }
+            });
+    }
+}
