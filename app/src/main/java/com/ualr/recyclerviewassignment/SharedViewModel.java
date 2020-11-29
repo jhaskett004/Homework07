a35c0fc001c9c86d0f12da6434b5fac32eb89af3
jhaskett004 <54286831+jhaskett004@users.noreply.github.com>
2020-11-29 17:41:42 -0600

    whatever

--- app/src/main/java/com/ualr/recyclerviewassignment/SharedViewModel.java
+++ app/src/main/java/com/ualr/recyclerviewassignment/SharedViewModel.java
@@ -11,8 +11,17 @@ import java.util.List;
 public class SharedViewModel extends ViewModel {
 
     private MutableLiveData<List<Inbox>> items;
+    private AdapterListInbox mAdapter;
+    ViewModel mViewModel;
 
     public SharedViewModel() {items = new MutableLiveData<>();}
+
+    mViewModel.getItems().observe(this, new Observer<List<Inbox>>() {
+        @Override
+        public void onChanged(List<Inbox> items) {
+            mAdapter.updateItems(items);
+        }
+    });
 
 
 }
